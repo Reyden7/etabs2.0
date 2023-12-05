e46cbe2bc20ac4930800ee2e0d39e25f4d491e93
reyden <quentin.cordiero@neuf.fr>
2023-12-05 14:17:58 +0100

    interface de connexion

--- Etabs2/App.js
+++ Etabs2/App.js
@@ -1,15 +1,45 @@
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, { useState,useEffect } from 'react';
 import Login from './app/screens/Login';
+import List from './app/screens/List';
+import Details from './app/screens/Details';
+import {User, onAuthStateChanged} from 'firebase/auth';
+import { FIREBASE_AUTH } from './FirebaseConfig';
 
 const Stack = createNativeStackNavigator();
+const InsideStack = createNativeStackNavigator();
 
+function InsideLayout()
+{
+  return (
+      <InsideStack.Navigator>
+        <InsideStack.Screen name="My Etabs" component={List}/>
+        <InsideStack.Screen name="Details" component={Details}/>
+      </InsideStack.Navigator> 
+  );
+}
 export default function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log('user', user);
+      setUser(user);
+    });
+     // Nettoyage de l'effet lors du démontage du composant
+  return () => unsubscribe();
+  },[]);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name="Login" component={Login} options={{headerShown : false }}/>
+        {user ? (
+          <Stack.Screen name="Inside" component={InsideLayout} options={{headerShown : false }}/>
+        ) : (
+          <Stack.Screen name="Login" component={Login} options={{headerShown : false }}/>
+        ) }
       </Stack.Navigator> 
     </NavigationContainer>
   );
--- Etabs2/app/screens/Login.tsx
+++ Etabs2/app/screens/Login.tsx
@@ -1,12 +1,112 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, ActivityIndicator, Button, Pressable, KeyboardAvoidingView } from 'react-native';
+import React, { useState } from 'react';
+import { FIREBASE_AUTH } from '../../FirebaseConfig';
+import { async } from '@firebase/util';
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
 
 const Login = () => {
-  return (
-    <View>
-      <Text>Login</Text>
-    </View>
-  )
-}
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const auth = FIREBASE_AUTH;
 
-export default Login
+  const handleLogin = async () => {
+    // Ajoutez ici la logique de connexion avec Firebase
+    console.log("connection")
+    console.log(email);
+    console.log(password);
+    setLoading(true);
+    try{
+        const response = await signInWithEmailAndPassword(auth,email,password);
+        console.log(response);
+        alert('Check your email !');
+    }catch(error : any){
+        console.log(error);
+        alert('Sign In failed !' + error.message);
+    }finally{
+        setLoading(false);
+    }
+
+  };
+
+  const SignUp = async () => {
+    // Ajoutez ici la logique de connexion avec Firebase
+    console.log("connection")
+    console.log(email);
+    console.log(password);
+    setLoading(true);
+    try{
+        const response = await createUserWithEmailAndPassword(auth,email,password);
+        console.log(response);
+        alert('Check your email !');
+    }catch(error : any){
+        console.log(error);
+        alert('Sign In failed !' + error.message);
+    }finally{
+        setLoading(false);
+    }
+
+  };
+
+  return (
+    <View style={styles.container}>
+    
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={(text) => setEmail(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
+     
+        {loading ? <ActivityIndicator size="large" color="#0000ff" />
+        : <>
+        <Pressable style={styles.loginButton} onPress={ handleLogin}><Text style={styles.buttonText}>Login </Text></Pressable>
+        <Pressable style={{backgroundColor: '#0000', marginTop:20}} onPress={ SignUp}><Text style={{color:'#007BFF'}}>Create account </Text></Pressable>
+        </>}
+        
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    height: 40,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 16,
+    paddingHorizontal: 10,
+  },
+  loginButton: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+    width: '100%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
